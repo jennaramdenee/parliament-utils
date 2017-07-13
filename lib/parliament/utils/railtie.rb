37dd65922a31cb8501482ffb7e7b7f36922bed44
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-07-13 15:13:44 +0100

    Moved initializers from prototype

--- lib/parliament/utils/railtie.rb
+++ lib/parliament/utils/railtie.rb
@@ -4,6 +4,7 @@ module Parliament
   module Utils
     class Railtie < Rails::Railtie
       initializer 'parliament-utils.view_helpers' do
+        require 'parliament/utils/config/initializers'
         ActionView::Base.send(:include, Helpers::ApplicationHelper)
       end
     end
