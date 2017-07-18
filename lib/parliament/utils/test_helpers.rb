c8e7936b10d066b38d383bc3151cfd3e7fe965ad
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-07-18 16:23:51 +0100

    use defined?(::Rails) instead of defined?(Rails)

--- lib/parliament/utils/test_helpers.rb
+++ lib/parliament/utils/test_helpers.rb
@@ -1,5 +1,5 @@
+require_relative './test_helpers/rails_helper' if defined?(::Rails)
 require_relative './test_helpers/rspec_helper'
-require_relative './test_helpers/rails_helper' if defined?(Rails)
 require_relative './test_helpers/simplecov_helper'
 require_relative './test_helpers/translation_helper'
 require_relative './test_helpers/vcr_helper'
@@ -10,8 +10,8 @@ module Parliament
   module Utils
     module TestHelpers
 
+      include Parliament::Utils::TestHelpers::RailsHelper if defined?(::Rails)
       include Parliament::Utils::TestHelpers::RSpecHelper
-      include Parliament::Utils::TestHelpers::RailsHelper if defined?(Rails)
       include Parliament::Utils::TestHelpers::SimpleCovHelper
       include Parliament::Utils::TestHelpers::TranslationHelper
       include Parliament::Utils::TestHelpers::VCRHelper
